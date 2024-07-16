ed782823cfc38bf8a11bfc252dd712d4e16e5829
tojo-joseph <tojo.joseph18@gmail.com>
2024-07-17 01:25:21 +0530

    new prices after discount added but total in progress

---
+++ main.py
@@ -0,0 +1,33 @@
+import functools
+# calculating the total cost after applying a discount for certain flavors.
+ice_cream_price_list = [('vanilla', 40),('strawberry', 50),('mango', 60),('butterscotch', 70),('chocolate', 100),('cookies_n_creme', 120),('blueberry_muffin', 140),('watermelon',  160)]
+
+# map to apply a discount of 10% to the prices of watermelon, mango, blueberry_muffin and butterscotch
+
+discounted_flavors = [
+    'watermelon',
+    'mango',
+    'blueberry_muffin',
+    'butterscotch'
+]
+    
+def add(sum_so_far, num):
+    return sum_so_far + num
+
+def discount_applier(ice_list):
+    filtered_ice_list = list(filter(discount_checker, ice_list))
+    print(f"Filtered Ice List: {filtered_ice_list}")
+    print(f"Filtered Ice List: {list(map(price_calculator, filtered_ice_list))}")
+    new_prices = list(map(price_calculator, filtered_ice_list))
+    return functools.reduce(add, new_prices)
+
+def price_calculator(x):
+    return int(x[1] - x[1] * 0.1)
+
+def discount_checker(item):
+    print(f"Item: {item[0] in discounted_flavors}")
+    return item[0] in discounted_flavors
+
+discount_applier(ice_cream_price_list)
+
+
